9eaac906a34da554afda027ebf723c8ad49bd050
liuych65 <1035105238@qq.com>
2020-11-21 17:11:06 +0800

    dqn-breakout-main_dueling

---
+++ dqn-breakout-main_dueling/utils_types.py
@@ -0,0 +1,34 @@
+"""
+Aliases created in this module are useless for static type checking, instead,
+they act as hints for human only
+"""
+#作用是给相应的变量赋予类型，any表示任意自定义的类型均可以。
+from typing import (
+    Any,
+)
+
+# Tensor with shape (None, 4, 84, 84)
+BatchState = Any
+# Tensor with shape (None, 1)
+BatchAction = Any
+# Tensor with shape (None, 1)
+BatchReward = Any
+# Tensor with shape (None, 4, 84, 84)
+BatchNext = Any
+# Tensor with shape (None, 1)
+BatchDone = Any
+# NDArray with shape (210, 160, 3)
+GymImg = Any
+# NDArray with shape (84, 84, 1)
+GymObs = Any
+# Tensor with shape (N, 1)
+TensorN1 = Any
+# Tensor with shape (1, 84, 84)
+TensorObs = Any
+# A stack with 4 GymObs, with shape (1, 4, 84, 84)
+TensorStack4 = Any
+# A stack with 5 GymObs, with shape (1, 5, 84, 84)
+TensorStack5 = Any
+# torch.device("cpu") or torch.device("cuda"), can be conditional on
+# torch.cuda.is_available()
+TorchDevice = Any
